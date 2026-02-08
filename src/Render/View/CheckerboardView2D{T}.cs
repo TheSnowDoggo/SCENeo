3023c0a551786967e6278f3a7fe71ad990be3929
Luna Bl3p <redstonewolfer@Gmail.com>
2026-02-08 13:56:50 +0000

    cc

---
+++ src/Render/View/CheckerboardView2D{T}.cs
@@ -0,0 +1,25 @@
+﻿namespace SCENeo;
+
+public sealed class CheckerboardView2D<T> : IView<T>
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Vec2I CheckSize { get; set; } = Vec2I.One;
+
+    public T Value { get; set; }
+    public T AltValue { get; set; }
+
+    public T this[int x, int y]
+    {
+        get
+        {
+            int checkX = Math.Abs(x / CheckSize.X % 2);
+            int checkY = Math.Abs(y / CheckSize.Y % 2);
+
+            return checkX == checkY ? Value : AltValue;
+        }
+    }
+
+    public T this[Vec2I coord] => this[coord.X, coord.Y];
+}
--- src/Render/View/PlainView2D{T}.cs
+++ src/Render/View/PlainView2D{T}.cs
@@ -8,11 +8,11 @@ namespace SCENeo;
 /// <typeparam name="T"></typeparam>
 public sealed class PlainView2D<T> : IView<T>
 {
-    public T Value { get; set; } = default!;
-
     public int Width { get; set; }
     public int Height { get; set; }
 
+    public T Value { get; set; }
+
     public T this[int x, int y] => Value;
-    public T this[Vec2I pos] => Value;
+    public T this[Vec2I coord] => Value;
 }
